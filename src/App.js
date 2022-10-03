1aa232809a8fe554ba5d56ac3de50e05d7b65977
JustineDev00 <dev.justine.verin@gmail.com>
2022-10-03 14:46:26 +0200

    Formulaire OK liens OK

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link, useNavigate} from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import LoggedScreen from './screens/LoggedScreen';
@@ -9,9 +9,21 @@ import AccountScreen from './screens/AccountScreen';
 import AdminScreen from './screens/AdminScreen';
 
 function App() {
+  
+
+
   return (
-    <div className="App">
+    <div className="App container container-fluid">
+
+
         <BrowserRouter>
+        <navbar className='d-flex w-100 justify-content-center my-3'>
+          <Link to='/' className='btn btn-primary mx-3'>Home</Link>
+          <Link to='/login' className='btn btn-primary mx-3'>Login</Link>
+          <Link to='/admin' className='btn btn-primary mx-3'>Admin</Link>
+          <Link to='/logged' className='btn btn-primary mx-3'>Logged</Link>
+          <Link  to='/account' className='btn btn-primary mx-3'>Account</Link>
+      </navbar>
           <Routes>
             <Route path='/' element={<HomeScreen/>}></Route>
             <Route path='/login' element={<LoginScreen/>}></Route>
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import {React, useRef} from 'react';
 
 const LoginScreen = () => {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+    
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({
+            login : emailRef.current.value,
+            password : passwordRef.current.value
+        });
+
+
+    }
+
+
+
     return (
-        <div>
-            Page LoginScreen
+        <div className='d-flex flex-column'>
+           <h1>Page LoginScreen</h1> 
+            <form method='POST'>
+                <div className='d-flex flex-column align-items-start'>
+                    <label htmlFor='login' className='form-label'>Email adress:</label>
+                    <input ref={emailRef} type="email" name="login" id="login" className='form-control w-50' />
+                </div>
+                <div className='d-flex flex-column align-items-start'>
+                    <label htmlFor='login' className='form-label'>Password:</label>
+                    <input ref={passwordRef} type="password" name="password" id="password" className='form-control w-50'/>
+                    <input type="submit" value="Submit" className='btn btn-primary mt-3'onClick={handleSubmit}/>
+                </div>
+            </form>
+
+
         </div>
     );
 };
